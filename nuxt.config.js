ad8ae52b5eb3d7c6e2a66843a138f171ebc8e6c5
Innocent Greats <97280792+innocent-greats@users.noreply.github.com>
2025-03-11 22:31:20 +0200

    sync

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,36 +3,44 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   ssr: false,
   modules: [
-    '@nuxtjs/supabase',
-    '@nuxt/icon',
-    '@nuxt/ui'
+    "vuetify-nuxt-module",
+    "@nuxtjs/supabase",
+    "@nuxt/icon",
+    "@nuxt/ui",
+    "vuetify-nuxt-module",
   ],
+  vuetify: {
+    moduleOptions: {
+      /* module specific options */
+    },
+    vuetifyOptions: {
+      /* vuetify options */
+    },
+  },
   runtimeConfig: {
     public: {
       middlewareOptions: {
-        loginRoute: '/auth/login',
-        registerRoute: '/auth/register',
-        authenticatedBasePath: '/user/dashboard',
-      }
-    }
+        loginRoute: "/auth/login",
+        registerRoute: "/auth/register",
+        authenticatedBasePath: "/user/dashboard",
+      },
+    },
   },
   supabase: {
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY,
     clientOptions: {
       auth: {
-        flowType: 'pkce',
+        flowType: "pkce",
         detectSessionInUrl: true,
         persistSession: true,
-        autoRefreshToken: true
+        autoRefreshToken: true,
       },
     },
     redirect: false,
   },
   router: {
-    linkExactActiveClass: ''
+    linkExactActiveClass: "",
   },
-  css: [
-    '~/assets/css/fontface.css'
-  ],
-})
+  css: ["~/assets/css/fontface.css"],
+});
